78d52107089e91833b875499272934f1cc4f7ede
Luu Huynh Duc <huynhduc12a8@gmail.com>
2023-05-29 00:07:37 +0700

    123

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from log.server import server_run, send_message, socketio
 from multiprocessing.pool import ThreadPool
 import time
 def main():
-    #pool = ThreadPool(200)
-    #pool.map_async(run, accounts)
+    pool = ThreadPool(200)
+    pool.map_async(run, accounts)
     while True:
         send_message(accounts)
         time.sleep(1)
